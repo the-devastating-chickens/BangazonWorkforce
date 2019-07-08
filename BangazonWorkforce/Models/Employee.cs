40ddb6b7574f00fe5dbe8fe5722e2184cea5c9b7
Billy Mathison <wlmathison@gmail.com>
2019-07-08 15:59:38 -0500

    Adding models for Employee, Department, Computer, and TrainingProgram

---
+++ BangazonWorkforce/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool IsSuperVisor { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public List<TrainingProgram> EmployeeTrainingPrograms { get; set; } = new List<TrainingProgram>();
+
+        public List<Computer> Computers { get; set; } = new List<Computer>();
+
+        public Department Department { get; set; }
+    }
+}
---
+++ BangazonWorkforce/Models/TrainingProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class TrainingProgram
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public int MaxAttendees { get; set; }
+
+        List<Employee> EmployeesInTrainingProgram { get; set; } = new List<Employee>();
+    }
+}
