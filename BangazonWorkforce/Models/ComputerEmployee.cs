73ab3dc0fc16a6a7fe2107829e6c8cda4e59190a
Billy Mathison <wlmathison@gmail.com>
2019-07-10 09:20:48 -0500

    Creating ComputerEmployee model

---
+++ BangazonWorkforce/Models/ComputerEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class ComputerEmployee
+    {
+        public DateTime AssignDate { get; set; }
+
+        public DateTime UnassignDate { get; set; }
+    }
+}
